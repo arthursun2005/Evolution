cc991a903b784dc6d1109e0cb470013593d5bc10
Arthur Sun <arthursun2005@gmail.com>
2019-05-31 12:01:42 +1000

    Initial Commit

---
+++ Evolution/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Evolution
+//
+//  Created by Arthur Sun on 5/31/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
