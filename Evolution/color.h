9fc1cee512f194c2d3d56dfd67c279ab62ab9918
Arthur Sun <arthursun2005@gmail.com>
2019-06-20 20:44:43 +1000

    colour hex

--- Evolution/color.h
+++ Evolution/color.h
@@ -9,6 +9,8 @@
 #ifndef color_h
 #define color_h
 
+#include <unistd.h>
+
 struct Colorf
 {
     float r;
@@ -25,6 +27,19 @@ struct Colorf
     Colorf(float r, float g, float b) : r(r), g(g), b(b), a(1.0f) {}
     
     Colorf(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
+    
+    inline uint32_t hex() const {
+        int _r = r * 256.0f + 0.5f;
+        int _g = g * 256.0f + 0.5f;
+        int _b = b * 256.0f + 0.5f;
+        int _a = a * 256.0f + 0.5f;
+        return (_r << 24) | (_g << 16) | (_b << 8) | _a;
+    }
+    
+    inline float compress() const {
+        uint32_t _hex = hex();
+        return *(float*)&_hex;
+    }
 };
 
 #endif /* color_h */
